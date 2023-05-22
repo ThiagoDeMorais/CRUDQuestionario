85377bd2fba9f877bba86d0aa5158db94066f8ed
Thigo De Morais <thiagodevelopermorais@gmail.com>
2023-05-22 17:29:52 -0300

    Demo - deletar dados

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,11 +2,10 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 import db.DB;
+import db.DbIntegrityException;
 
 public class Program {
 
@@ -14,29 +13,22 @@ public class Program {
 
 		Connection conn = null;
 		PreparedStatement st = null;
-		
+
 		try {
 			conn = DB.getConnection();
-			st = conn.prepareStatement("UPDATE questao "
-					+ "SET enunciado = ? "
-					+ "WHERE "
-					+ "(id_questao = ?)");
-			
-			st.setString(1, "Não leibe");
-			st.setInt(2, 7);
-			
+			st = conn.prepareStatement("DELETE FROM questao " + "WHERE " + "id_questao = ?");
+
+			st.setInt(1, 7);
+
 			int rowsAffected = st.executeUpdate();
 			System.out.println("Pronto! Quantidade de linhas alteradas:" + rowsAffected);
-		
-		}catch(SQLException e) {
-			e.printStackTrace();
-		}finally {
+
+		} catch (SQLException e) {
+			throw new DbIntegrityException(e.getMessage());
+		} finally {
 			DB.closeStatement(st);
 			DB.closeConnection();
 		}
-
-	
-		
 
 	}
 
